e1e057a1f486cf58a01349fcef954d15eb028688
Leonardo Carmona <leonardo.carmona@cwi.com.br>
2017-06-22 19:08:17 -0300

    Small fix in javadoc

--- src/main/java/com/github/qacore/testingtoolbox/configuration/selenium/AbstractWebDriverConfiguration.java
+++ src/main/java/com/github/qacore/testingtoolbox/configuration/selenium/AbstractWebDriverConfiguration.java
@@ -14,8 +14,8 @@ import com.github.qacore.testingtoolbox.selenium.parallel.WebDriverManager;
  * 
  * @author Leonardo Carmona da Silva
  *         <ul>
- *         <li><a href= "https://br.linkedin.com/in/l3ocarmona">https://br.linkedin.com/in/l3ocarmona</a></li>
- *         <li><a href= "https://github.com/leocarmona">https://github.com/leocarmona</a></li>
+ *         <li><a href="https://br.linkedin.com/in/l3ocarmona">https://br.linkedin.com/in/l3ocarmona</a></li>
+ *         <li><a href="https://github.com/leocarmona">https://github.com/leocarmona</a></li>
  *         <li><a href="mailto:lcdesenv@gmail.com">lcdesenv@gmail.com</a></li>
  *         </ul>
  * 
