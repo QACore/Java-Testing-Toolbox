96d69b51f47a906935f2b24f78dcde932b03ec5e
Leonardo Carmona <leonardo.carmona@cwi.com.br>
2017-07-18 03:55:27 -0300

    Image improvement

--- src/main/java/com/github/qacore/testingtoolbox/selenium/elements/Image.java
+++ src/main/java/com/github/qacore/testingtoolbox/selenium/elements/Image.java
@@ -1,5 +1,10 @@
 package com.github.qacore.testingtoolbox.selenium.elements;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -58,6 +63,52 @@ public class Image extends BasicElement {
      */
     public String getSrc() {
         return this.getAttribute("src");
+    }
+
+    /**
+     * Download the image to specified path.
+     * 
+     * @param pathname
+     *            A pathname string.
+     * 
+     * @param connectionTimeout
+     *            The number of milliseconds until this method will timeout if no connection could be established to the <code>source</code>.
+     * 
+     * @param readTimeout
+     *            The number of milliseconds until this method will timeout if no data could be read from the <code>source</code>.
+     * 
+     * @return The file.
+     */
+    public File downloadImage(String pathname, int connectionTimeout, int readTimeout) {
+        File file = new File(pathname);
+
+        try {
+            FileUtils.copyURLToFile(new URL(this.getSrc()), file, connectionTimeout, readTimeout);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return file;
+    }
+
+    /**
+     * Download the image to specified path.
+     * 
+     * @param pathname
+     *            A pathname string.
+     * 
+     * @return The file.
+     */
+    public File downloadImage(String pathname) {
+        File file = new File(pathname);
+
+        try {
+            FileUtils.copyURLToFile(new URL(this.getSrc()), file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return file;
     }
 
 }
